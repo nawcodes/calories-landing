0415821bc005e08b744bc0c28351790976747ecb
Nawcodes <nawcodes@gmail.com>
2022-05-28 15:27:35 +0700

    delete user_guide

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/nidaweb';
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://calories-landing.test/';
 
 /*
 |--------------------------------------------------------------------------
