748941a8c6733f96b4bb7f4799b9d22c0b17d7af
Rifal Nurjamil <81290043+nawcodes@users.noreply.github.com>
2022-06-26 18:15:45 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/nidaweb';
-$config['base_url'] = 'http://calories-landing.test/';
+// $config['base_url'] = 'http://calories-landing.test/';
+$config['base_url'] = 'http://localhost/calories-landing-main';
 
 /*
 |--------------------------------------------------------------------------
